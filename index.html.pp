66e4b5f82053bf5977b3b36ac5c0e243167d8298
Leandro Ostera <leandro@ostera.io>
2016-07-03 20:11:05 +0200

    Interpolation!

--- index.html.pp
+++ index.html.pp
@@ -4,7 +4,8 @@
 ◊(define edge (* inside 2))
 ◊(define color "blue")
 
-This is some text.
-This is some otter.
-That was a joke!
-Oh well don't bother...
+; stick to the tutorial for once
+The margin is ◊|edge|em.
+The border is ◊|color|.
+The padding is ◊|inside|em.
+The brder is too.
