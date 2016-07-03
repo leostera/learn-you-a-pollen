879511e795ce07f864946cdf0c2257458e9b91c7
Leandro Ostera <leandro@ostera.io>
2016-07-03 20:33:44 +0200

    Ignore markdowns, force add README

--- index.html.pp
+++ index.html.pp
@@ -4,7 +4,6 @@
 ◊(define edge (* inside 2))
 ◊(define color "blue")
 
-; stick to the tutorial for once
 The margin is ◊|edge|em.
 The border is ◊|color|.
 The padding is ◊|inside|em.
---
+++ list.md.pp
@@ -0,0 +1,11 @@
+#lang pollen
+
+◊(define what-to-do "laundry")
+◊(define what-to-buy "pancakes")
+◊(define another-one "workout")
+
+This is a _list_:
+
+1. Do ◊what-to-do
+2. Buy ◊what-to-buy
+3. ◊another-one
